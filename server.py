a9afbc02443568305c6b0dc5fe92853958f146d1
KirillYabl <kirill-yablunovskii@mail.ru>
2022-12-23 17:22:59 +0300

    Add logging while download

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import asyncio
+import logging
 import os
 
 from aiohttp import web
 from aiohttp.web_request import StreamResponse
 import aiofiles
 
-DATA_CHUNK = 1024 * 100
+logger = logging.getLogger(__name__)
+
+DATA_CHUNK = 1024 * 100  # 100 kb
 
 
 async def archive(request: web.Request) -> StreamResponse:
@@ -24,9 +27,12 @@ async def archive(request: web.Request) -> StreamResponse:
 
     await response.prepare(request)
 
+    chunk_number = 1
     while not proc.stdout.at_eof():
         data = await proc.stdout.read(DATA_CHUNK)
+        logger.debug(f'Sending archive chunk {chunk_number} ...')
         await response.write(data)
+        chunk_number += 1
 
     return response
 
@@ -38,6 +44,7 @@ async def handle_index_page(request: web.Request) -> web.Response:
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
